de1632d68038db78c55ad2a0183989ee3e8e8b0d
nbmjhhhj <nsjcongyeen2549@hotmail.com>
2024-05-08 13:26:42 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-function permuteUnique(nums) {
-  nums.sort((a, b) => a - b);
-  const result = [];
-  const used = new Array(nums.length).fill(false);
-  backtrack([]);
-  return result;
-  function backtrack(permutation) {
-    if (permutation.length === nums.length) {
-      result.push([...permutation]);
-      return;
-    }
-    for (let i = 0; i < nums.length; i++) {
-      if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]))
-        continue;
-      used[i] = true;
-      permutation.push(nums[i]);
-      backtrack(permutation);
-      permutation.pop();
-      used[i] = false;
-    }
+function isAlienSorted(words, order) {
+  const dict = new Map();
+  for (let i = 0; i < order.length; i++) {
+    dict.set(order[i], i);
   }
+  for (let i = 0; i < words.length - 1; i++) {
+    const word1 = words[i];
+    const word2 = words[i + 1];
+    let found = false;
+    for (let j = 0; j < Math.min(word1.length, word2.length); j++) {
+      if (word1[j] !== word2[j]) {
+        if (dict.get(word1[j]) > dict.get(word2[j])) return false;
+        found = true;
+        break;
+      }
+    }
+    if (!found && word1.length > word2.length) return false;
+  }
+  return true;
 }
