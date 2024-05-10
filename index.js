79abe6dcb2955fd57636b02389a96dca02930434
nbmjhhhj <nsjcongyeen2549@hotmail.com>
2024-05-11 01:08:33 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-function wiggleSort(nums) {
-  nums.sort((a, b) => a - b);
-  const median = Math.floor((nums.length + 1) / 2);
-  const left = nums.slice(0, median);
-  const right = nums.slice(median);
-  for (let i = 0; i < nums.length; i++) {
-    if (i % 2 === 0) nums[i] = left.pop();
-    else nums[i] = right.pop();
+function mergeKLists(lists) {
+  if (!lists || !lists.length) return null;
+  return mergeLists(lists, 0, lists.length - 1);
+  function mergeLists(lists, start, end) {
+    if (start === end) return lists[start];
+    const mid = Math.floor((start + end) / 2);
+    const left = mergeLists(lists, start, mid);
+    const right = mergeLists(lists, mid + 1, end);
+    return mergeTwoLists(left, right);
+  }
+  function mergeTwoLists(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+    } else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2;
+    }
   }
 }
